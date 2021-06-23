e51a9c57b748f26b4bd47ccb877e61c02d7cfe7a
CDD <chengdidi@ibjcr.cn>
2021-06-23 15:26:51 +0800

    666

---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CDD
+ * Date: 2021/6/23
+ * Time: 15:25
+ */
+
+namespace hfcfb;
+
+
+class Test
+{
+    public function init()
+    {
+        echo "Hi, brother. I'm sorry, it's just demo from composer package test", PHP_EOL;
+    }
+}
